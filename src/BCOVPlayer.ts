9937eccdf80a8cb81a7a7196c49fff1034464d6d
Carlos Ceja Limón <cceja@brightcove.com>
2019-06-06 11:56:18 -0500

    testing

--- src/BCOVPlayer.ts
+++ src/BCOVPlayer.ts
@@ -2,28 +2,23 @@
 
 import { SkillBuilders } from 'ask-sdk-core';
 import { LambdaHandler } from 'ask-sdk-core/dist/skill/factory/BaseSkillFactory';
-import { BCOVPlaybackService } from './BCOVPlaybackService';
+import { BCOVPlaybackServiceData } from './BCOVPlaybackService';
 import { LaunchRequestHandler } from './LaunchRequestHandler';
 import { PlayRequestHandler } from './PlayRequestHandler';
 
 class BCOVPlayer {
-  public readonly accountId: string;
-  public readonly policyKey: string;
+    public readonly playbackService: BCOVPlaybackServiceData;
 
-  private readonly playbackService: BCOVPlaybackService;
+    constructor(playbackService: BCOVPlaybackServiceData) {
+        this.playbackService = playbackService;
+    }
 
-  constructor(accountId: string, policyKey: string) {
-    this.accountId = accountId;
-    this.policyKey = policyKey;
-    this.playbackService = new BCOVPlaybackService(this.accountId, this.policyKey);
-  }
-
-  public getLambda(): LambdaHandler {
-    return SkillBuilders.custom()
-      .addRequestHandlers(new LaunchRequestHandler(this.playbackService), new PlayRequestHandler())
-      .addErrorHandlers()
-      .lambda();
-  }
+    public getLambda(): LambdaHandler {
+        return SkillBuilders.custom()
+            .addRequestHandlers(new LaunchRequestHandler(this.playbackService), new PlayRequestHandler())
+            .addErrorHandlers()
+            .lambda();
+    }
 }
 
 export { BCOVPlayer };
