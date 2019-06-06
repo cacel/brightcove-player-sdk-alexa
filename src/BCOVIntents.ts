8ea4d770ba6a3d60b08c5d9bf7c45167e102dcd7
Carlos Ceja Limón <cceja@brightcove.com>
2019-06-06 14:02:10 -0500

    add intents

---
+++ src/BCOVIntents.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+export { BCOVPresentationIntent } from './BCOVIntents/BCOVPresentationIntent';
+export { BCOVSearchPlaylistIntent } from './BCOVIntents/BCOVSearchPlaylistIntent';
+export { BCOVSearchVideoIntent } from './BCOVIntents/BCOVSearchVideoIntent';
+export { BCOVSearchRelatedIntent } from './BCOVIntents/BCOVSearchRelatedIntent';
--- src/BCOVPlayer.ts
+++ src/BCOVPlayer.ts
@@ -4,21 +4,32 @@ import { SkillBuilders } from 'ask-sdk-core';
 import { LambdaHandler } from 'ask-sdk-core/dist/skill/factory/BaseSkillFactory';
 import { BCOVPlaybackServiceData } from './BCOVPlaybackService';
 import { LaunchRequestHandler } from './LaunchRequestHandler';
-import { PlayRequestHandler } from './PlayRequestHandler';
+import {
+  BCOVPresentationIntent,
+  BCOVSearchPlaylistIntent,
+  BCOVSearchVideoIntent,
+  BCOVSearchRelatedIntent,
+} from './BCOVIntents';
 
 class BCOVPlayer {
-    public readonly playbackService: BCOVPlaybackServiceData;
+  public readonly playbackService: BCOVPlaybackServiceData;
 
-    constructor(playbackService: BCOVPlaybackServiceData) {
-        this.playbackService = playbackService;
-    }
+  constructor(playbackService: BCOVPlaybackServiceData) {
+    this.playbackService = playbackService;
+  }
 
-    public getLambda(): LambdaHandler {
-        return SkillBuilders.custom()
-            .addRequestHandlers(new LaunchRequestHandler(this.playbackService), new PlayRequestHandler())
-            .addErrorHandlers()
-            .lambda();
-    }
+  public getLambda(): LambdaHandler {
+    return SkillBuilders.custom()
+      .addRequestHandlers(
+        new LaunchRequestHandler(this.playbackService),
+        new BCOVPresentationIntent(),
+        new BCOVSearchPlaylistIntent(),
+        new BCOVSearchVideoIntent(),
+        new BCOVSearchRelatedIntent(),
+      )
+      .addErrorHandlers()
+      .lambda();
+  }
 }
 
 export { BCOVPlayer };
